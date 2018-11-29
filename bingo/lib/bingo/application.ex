90248075490ea8676cc90df5169d2f59a9e04de6
danilomartins <dglmartins@outlook.com>
2018-11-29 15:24:33 -0500

    Gameserver, application, supervisor

---
+++ bingo/lib/bingo/application.ex
@@ -0,0 +1,15 @@
+defmodule Bingo.Application do
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      {Registry, keys: :unique, name: Bingo.GameRegistry},
+      Bingo.GameSupervisor
+    ]
+
+    :ets.new(:games_table, [:public, :named_table])
+
+    opts = [strategy: :one_for_one, name: Bingo.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
