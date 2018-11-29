18d7758c31430f0f5fd7908d564f57806cadd5d8
danilomartins <dglmartins@outlook.com>
2018-11-29 14:00:50 -0500

    mark square with name given phrase

--- bingo/lib/bingo/game.ex
+++ bingo/lib/bingo/game.ex
@@ -19,4 +19,33 @@ defmodule Bingo.Game do
 
     %Bingo.Game{squares: squares}
   end
+
+  @doc """
+  Marks the square that has the given `phrase` for the given `player`,
+  updates the scores, and checks for a bingo!
+  """
+  def mark(game, phrase, player) do
+    game
+    |> update_squares_with_mark(phrase, player)
+  end
+
+  defp update_squares_with_mark(game, phrase, player) do
+    new_squares =
+      game.squares
+      |> List.flatten()
+      |> Enum.map(&mark_square_having_phrase(&1, phrase, player))
+      |> Enum.chunk_every(Enum.count(game.squares))
+
+    %{game | squares: new_squares}
+  end
+
+  defp mark_square_having_phrase(%{phrase: phrase} = square, phrase, player) do
+    IO.puts("Im here")
+    %Square{square | marked_by: player}
+  end
+
+  defp mark_square_having_phrase(square, _phrase, _player) do
+    IO.puts("No Im here")
+    square
+  end
 end
