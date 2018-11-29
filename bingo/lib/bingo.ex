83bb49947bdb39462bab493eca2d15a21617ee1b
danilomartins <dglmartins@outlook.com>
2018-11-29 13:06:59 -0500

    clean bingo template and test

--- bingo/lib/bingo.ex
+++ bingo/lib/bingo.ex
@@ -1,18 +1,2 @@
 defmodule Bingo do
-  @moduledoc """
-  Documentation for Bingo.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Bingo.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- bingo/test/bingo_test.exs
+++ bingo/test/bingo_test.exs
@@ -1,8 +1,4 @@
 defmodule BingoTest do
   use ExUnit.Case
   doctest Bingo
-
-  test "greets the world" do
-    assert Bingo.hello() == :world
-  end
 end
