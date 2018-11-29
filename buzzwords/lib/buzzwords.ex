e177b7c481abaa57e2350de3363bea47c548e61d
danilomartins <dglmartins@outlook.com>
2018-11-29 11:54:59 -0500

    clean template and test

--- buzzwords/lib/buzzwords.ex
+++ buzzwords/lib/buzzwords.ex
@@ -1,18 +1,2 @@
 defmodule Buzzwords do
-  @moduledoc """
-  Documentation for Buzzwords.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Buzzwords.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- buzzwords/test/buzzwords_test.exs
+++ buzzwords/test/buzzwords_test.exs
@@ -1,8 +1,4 @@
 defmodule BuzzwordsTest do
   use ExUnit.Case
   doctest Buzzwords
-
-  test "greets the world" do
-    assert Buzzwords.hello() == :world
-  end
 end
