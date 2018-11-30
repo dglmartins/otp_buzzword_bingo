fb53e4f3fbb681deebda64576a04b29c1eb4ec67
danilomartins <dglmartins@outlook.com>
2018-11-29 21:28:48 -0500

    get names of games that are running

--- bingo/lib/bingo/game_supervisor.ex
+++ bingo/lib/bingo/game_supervisor.ex
@@ -20,7 +20,7 @@ defmodule Bingo.GameSupervisor do
   """
   def start_game(game_name, size) do
     child_spec = %{
-      id: GameServer, 
+      id: GameServer,
       start: {GameServer, :start_link, [game_name, size]},
       restart: :transient
     }
@@ -33,8 +33,17 @@ defmodule Bingo.GameSupervisor do
   """
   def stop_game(game_name) do
     :ets.delete(:games_table, game_name)
-    
+
     child_pid = GameServer.game_pid(game_name)
     DynamicSupervisor.terminate_child(__MODULE__, child_pid)
+  end
+
+  # get all games running
+  def game_names() do
+    DynamicSupervisor.which_children(__MODULE__)
+    |> Enum.map(fn {_, game_pid, _, _} ->
+      Registry.keys(Bingo.GameRegistry, game_pid) |> List.first()
+    end)
+    |> Enum.sort()
   end
 end
